a2a135e8e84be91b88b52451d922d732f3f66d56
Chanchikova Evgeniya <chanchikova_@mail.ru>
2024-02-06 23:34:48 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+Использовать рекурсию, не использовать циклы.
+*/
+void PrintMassiv(int[] array, int n = 1)
+{
+    if (n > array.Length)
+    {
+        return;
+    }
+    Console.Write($"{array[array.Length - n]} ");
+    PrintMassiv(array, n + 1);
+
+}
+int[] numbers = { 5, 6, 7, 8, 9, 10 };
+PrintMassiv(numbers);
